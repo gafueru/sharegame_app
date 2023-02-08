bd445e30eee3c80d1db4647a942629bc805c8c46
gafueru <gafuri1780@gmail.com>
2023-02-08 19:45:34 +0900

    userコントローラーの作成とshow.html.erbの記入

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'tweets/index'
   root to: 'tweets#index'
+  resources :users, only: :show
   resources :tweets, only: [:new, :create, :show]
 end
