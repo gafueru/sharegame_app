3dcb1ec5838b6e9da9c9e14bbc9e57c62db66e6b
gafueru <gafuri1780@gmail.com>
2023-02-09 17:42:08 +0900

    投稿者ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'tweets/index'
   root to: 'tweets#index'
   resources :users, only: :show
   resources :tweets, only: [:new, :create, :show, :edit, :update, :destroy ] do
