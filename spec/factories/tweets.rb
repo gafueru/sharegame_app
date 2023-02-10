55f0c9404d6bde35d61b38efaa924a62c216a48c
gafueru <gafuri1780@gmail.com>
2023-02-10 17:09:53 +0900

    Fakerの導入とuserのテストコードの記述

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -4,7 +4,5 @@ FactoryBot.define do
     text    {'text'}
   end
 
-  after(:build) do |tweet|
-    tweet.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-  end
+  
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name              {'test'}
     email                 {'test@example'}
-    password              {'000000'}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
   end
 end
